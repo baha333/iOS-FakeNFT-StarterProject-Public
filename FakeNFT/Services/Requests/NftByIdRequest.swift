3658ce1f666c6470cd5025820f563506330f3719
Баха <109457622+baha333@users.noreply.github.com>
2024-07-20 17:05:02 +0300

    Декомпозиция задач

--- FakeNFT/Services/Requests/NftByIdRequest.swift
+++ FakeNFT/Services/Requests/NftByIdRequest.swift
@@ -1,9 +1,7 @@
 import Foundation
 
 struct NFTRequest: NetworkRequest {
-
     let id: String
-
     var endpoint: URL? {
         URL(string: "\(RequestConstants.baseURL)/api/v1/nft/\(id)")
     }
